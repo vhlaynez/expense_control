5e85187076862e29624a1e12d97012ddd86e52ec
Victor Hugo <victor.laynez@ateliware.com>
2022-01-08 20:32:51 -0300

    Rubocop fixes

--- spec/models/expense_spec.rb
+++ spec/models/expense_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe Expense, type: :model do
       context 'when is greater than zero' do
         it 'validates without errors' do
           expense = described_class.new(title: 'test', date: DateTime.now,
-                                          amount_cents: 1)
+                                        amount_cents: 1)
           expect(expense).to be_valid(:amount_cents)
         end
       end
@@ -16,7 +16,7 @@ RSpec.describe Expense, type: :model do
       context 'when is less than 100_000_000' do
         it 'validates without errors' do
           expense = described_class.new(title: 'test', date: DateTime.now,
-                                          amount_cents: 100_000)
+                                        amount_cents: 100_000)
           expect(expense).to be_valid(:amount_cents)
         end
       end
@@ -26,7 +26,7 @@ RSpec.describe Expense, type: :model do
       context 'when is less than zero' do
         it 'returns error message' do
           expense = described_class.new(title: 'test', date: DateTime.now,
-                                          amount_cents: -1)
+                                        amount_cents: -1)
           expect(expense).not_to be_valid
           expect(expense.errors.messages).to include :amount_cents
         end
@@ -35,7 +35,7 @@ RSpec.describe Expense, type: :model do
       context 'when is greater than 100_000_000' do
         it 'returns error message' do
           expense = described_class.new(title: 'test', date: DateTime.now,
-                                          amount_cents: 100_000_001)
+                                        amount_cents: 100_000_001)
           expect(expense).not_to be_valid
           expect(expense.errors.messages).to include :amount_cents
         end
@@ -52,7 +52,7 @@ RSpec.describe Expense, type: :model do
       context 'when is not a number' do
         it 'returns error message' do
           expense = described_class.new(title: 'test', date: DateTime.now,
-                                          amount_cents: 'test not a number')
+                                        amount_cents: 'test not a number')
           expect(expense).not_to be_valid
           expect(expense.errors.messages).to include :amount_cents
         end
@@ -64,7 +64,7 @@ RSpec.describe Expense, type: :model do
     context 'when it is present' do
       it 'validates without errors' do
         expense = described_class.new(title: 'test', date: DateTime.now,
-                                        amount_cents: 1)
+                                      amount_cents: 1)
         expect(expense).to be_valid(:title)
       end
     end
@@ -82,7 +82,7 @@ RSpec.describe Expense, type: :model do
     context 'when it is a date' do
       it 'validates without errors' do
         expense = described_class.new(title: 'test', date: DateTime.now,
-                                        amount_cents: 1)
+                                      amount_cents: 1)
         expect(expense).to be_valid(:title)
       end
     end
@@ -90,7 +90,7 @@ RSpec.describe Expense, type: :model do
     context 'when it is not a date' do
       it 'returns error message' do
         expense = described_class.new(title: 'test', date: 'test date',
-                                        amount_cents: 1)
+                                      amount_cents: 1)
         expect(expense).not_to be_valid
         expect(expense.errors.messages).to include :date
       end
