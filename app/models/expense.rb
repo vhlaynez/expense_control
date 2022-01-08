1a5ce850c46fc0ef82cdbd08c44fcd90ec6a3fe8
Victor Hugo <victor.laynez@ateliware.com>
2022-01-08 20:29:22 -0300

    Added expense model

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,4 +1,14 @@
 # frozen_string_literal: true
 
 class Expense < ApplicationRecord
+  monetize :amount_cents, disable_validation: true
+  register_currency :brl
+
+  validates :amount_cents, presence: true, numericality: {
+    greater_than: 0,
+    less_than: 100_000_000,
+    message: 'Invalid data'
+  }
+  validates :title, presence: true
+  validates :date, date: true
 end
